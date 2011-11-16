aff08baff9506efa1733eb94903ff13845c7e059
Andrew Lamb <andrew@nerdnetworks.org>
2011-11-16 14:51:20 -0500

    Porter Stemmer

--- strings_package/src/ddl.sql
+++ strings_package/src/ddl.sql
@@ -4,6 +4,7 @@ select version();
 CREATE LIBRARY StringsLib AS :libfile;
 
 CREATE FUNCTION EditDistance                   AS LANGUAGE 'C++' NAME 'EditDistanceFactory'         LIBRARY StringsLib;
+CREATE FUNCTION Stemmer                        AS LANGUAGE 'C++' NAME 'PorterStemmerFactory'        LIBRARY StringsLib;
 CREATE TRANSFORM FUNCTION StringTokenizer      AS LANGUAGE 'C++' NAME 'StringTokenizerFactory'      LIBRARY StringsLib;
 CREATE TRANSFORM FUNCTION StringTokenizerDelim AS LANGUAGE 'C++' NAME 'StringTokenizerDelimFactory' LIBRARY StringsLib;
 
