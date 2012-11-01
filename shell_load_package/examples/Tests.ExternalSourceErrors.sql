0ee0c86cfbe0b9ebc68eda5f515cf9595ba1787b
Jaka Jančar <jaka@celtra.com>
2012-11-01 23:22:05 +0100

    Tests for echo to stderr + exit. Code does not pass yet.

--- shell_load_package/examples/Tests.ExternalSourceErrors.sql
+++ shell_load_package/examples/Tests.ExternalSourceErrors.sql
@@ -7,4 +7,6 @@ copy t with source ExternalSource(cmd='exit 7') no commit;
 
 copy t with source ExternalSource(cmd='echo "message on stderr" >&2') no commit;
 
+copy t with source ExternalSource(cmd='echo "message on stderr and exit" >&2; exit 8') no commit;
+
 drop table t;
