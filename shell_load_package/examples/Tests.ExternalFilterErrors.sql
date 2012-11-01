ca52801d48037e4385433ac159cae7529aa91114
Jaka Jančar <jaka@celtra.com>
2012-11-01 11:36:18 +0100

    Separating error tests into different files

---
+++ shell_load_package/examples/Tests.ExternalFilterErrors.sql
@@ -0,0 +1,12 @@
+\! seq 1 10 > $TMPDIR/sample.dat
+\set datafile '\''`echo $TMPDIR`'/sample.dat\''
+create table t (i integer);
+
+copy t from :datafile with filter ExternalFilter(cmd='cat') no commit;
+
+copy t with source ExternalSource(cmd='exit 7') no commit;
+
+copy t with source ExternalSource(cmd='echo "message on stderr" >&2') no commit;
+
+drop table t;
+\! rm $TMPDIR/sample.dat
---
+++ shell_load_package/examples/Tests.ExternalSourceErrors.sql
@@ -0,0 +1,10 @@
+
+create table t (i integer);
+
+copy t with source ExternalSource(cmd='seq 1 10') no commit;
+
+copy t with source ExternalSource(cmd='exit 7') no commit;
+
+copy t with source ExternalSource(cmd='echo "message on stderr" >&2') no commit;
+
+drop table t;
