c22a8c94b5a1d7b94590aba9c000816973dda8a9
Kemal Hadimli <kemal@hadimli.com>
2014-05-27 16:34:59 +0300

    Allow variable number of partition and order keys

--- nth_value/src/NthValue.cpp
+++ nth_value/src/NthValue.cpp
@@ -19,10 +19,7 @@ public:
 			AnalyticPartitionReader &arg_reader,
 			AnalyticPartitionWriter &res_writer)
 	{
-		// Basic error checking
-		if (arg_reader.getNumCols() != 4)
-			vt_report_error(0, "Function accepts 2 arguments, but %zu provided",
-							arg_reader.getNumCols() - 2);
+		vint num_cols = arg_reader.getNumCols(); // first few columns are partition and order keys
 
 		vint count = 0;
 		vint isFound = 0;
@@ -31,8 +28,8 @@ public:
 		// While we have inputs to process
 		do {
 			// Get a copy of the input expression
-			std::string expr = arg_reader.getStringRef(2).str();
-			const vint offset = arg_reader.getIntRef(3);
+			std::string expr = arg_reader.getStringRef(num_cols - 2).str(); // last-1st arg is expression
+			const vint offset = arg_reader.getIntRef(num_cols - 1); // last arg is offset
 
 			if (offset < 1) {
 				vt_report_error(0, "This function only accepts positive integers as offset");
@@ -81,7 +78,9 @@ class NthValueFactory : public AnalyticFunctionFactory
 							const SizedColumnTypes &argTypes,
 							SizedColumnTypes &returnType)
 	{
-		const VerticaType &t = argTypes.getColumnType(2);
+		vint num_cols = argTypes.getColumnCount(); // first few columns are partition and order keys
+
+		const VerticaType &t = argTypes.getColumnType(num_cols - 2); // expression arg
 		returnType.addVarchar(t.getStringLength());
 	}
 
