7e9396ffa1515d45927f0cde08855e4f4867d777
h-serizawa <hibiki.serizawa@vertica.com>
2022-03-24 16:42:22 +0900

    ConnectBy supports VARCHAR inputs.

--- compatlib_functions/ddl/install.sql
+++ compatlib_functions/ddl/install.sql
@@ -3,7 +3,8 @@ select version();
 \set libfile '\''`pwd`'/lib/CompatLib.so\'';
 CREATE LIBRARY CompatLib AS :libfile;
 
-CREATE TRANSFORM FUNCTION connect_by_path AS LANGUAGE 'C++' NAME 'ConnectByFactory' LIBRARY CompatLib;
+CREATE TRANSFORM FUNCTION connect_by_path AS LANGUAGE 'C++' NAME 'ConnectByIntFactory' LIBRARY CompatLib;
+CREATE TRANSFORM FUNCTION connect_by_path AS LANGUAGE 'C++' NAME 'ConnectByVarcharFactory' LIBRARY CompatLib;
 CREATE TRANSFORM FUNCTION transpose       AS LANGUAGE 'C++' NAME 'TransposeFactory' LIBRARY CompatLib;
 
 -- Add more as needed
@@ -11,4 +12,3 @@ CREATE TRANSFORM FUNCTION group_generator_3 AS LANGUAGE 'C++' NAME 'GroupGenerat
 CREATE TRANSFORM FUNCTION group_generator_3 AS LANGUAGE 'C++' NAME 'GroupGeneratorFactoryFVF' LIBRARY CompatLib;
 CREATE TRANSFORM FUNCTION group_generator_4 AS LANGUAGE 'C++' NAME 'GroupGeneratorFactoryVVVV' LIBRARY CompatLib;
 CREATE TRANSFORM FUNCTION group_generator_4 AS LANGUAGE 'C++' NAME 'GroupGeneratorFactoryFVFV' LIBRARY CompatLib;
-
