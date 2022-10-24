0285e3e38dbd808e8709be1d2b03bdfb5e28b754
David Mankins <david.mankins@microfocus.com>
2022-10-24 14:49:27 -0400

    add copyright

--- bandpass/highpass_run.sql
+++ bandpass/highpass_run.sql
@@ -1,3 +1,19 @@
+-- See README.md to see what this script is all about
+
+-- (c) Copyright [2022] Micro Focus or one of its affiliates.
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- You may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+-- http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+
 DROP TABLE IF EXISTS highpass_out;
 CREATE TABLE highpass_out AS
 SELECT highpass_data_with_deltas.row_n as row_n,
